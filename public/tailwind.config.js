a4b2f6eb2e4988592fe1335d5da4a23ea4cf6fca
Kalpesh Lohar <loharkalpesh8@gmail.com>
2022-07-23 21:28:06 +0530

    Fixed some changes

---
+++ public/tailwind.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: "#2BC3FC",
+      "primar-second": "#017EAC",
+      "dark-blue": "#00103C",
+      white: "#FFFFFF",
+      red: "#ff0000",
+      yellow: "#F2994A",
+      grey: "#798196",
+      chipGrey: "#EFEFEF",
+      green: "#1DFF4E",
+
+      textBlack: "#00103ccc",
+    },
+    minHeight: {
+      mentor: "35vh",
+      half: "58vh",
+      mentorbox: "350px",
+    },
+    minWidth: {
+      box: "150px",
+      mentor: "25vh",
+      mentorbox: "350px",
+    },
+    maxHeight: {
+      mentorbox: "350px",
+    },
+    maxWidth: {
+      threethirty: "330px",
+      md: "450px",
+      lg: "600px",
+      box: "150px",
+      mentorbox: "550px",
+      packagebox: "400px",
+    },
+    fontFamily: {
+      primayfont: ["Poppins", "sans-serif"],
+    },
+    border: {
+      1: "1px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"), // add this to your plugins
+  ],
+};
